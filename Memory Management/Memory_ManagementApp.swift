b636211ab8bb9facfdaee10bfc64d152bcc5fb89
Hannes Nagel <hannes.nagel@codeunity.de>
2025-04-24 15:31:28 +0200

    Initial Commit

---
+++ Memory Management/Memory_ManagementApp.swift
@@ -0,0 +1,17 @@
+//
+//  Memory_ManagementApp.swift
+//  Memory Management
+//
+//  Created by Hannes Nagel on 4/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Memory_ManagementApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
